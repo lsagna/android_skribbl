5a2cca2a49fd6f10293e49bf8b68bc36123ccb84
unknown <lsagna1@myges.fr>
2021-02-11 03:56:05 +0100

    Image sender

--- app/src/main/java/com/example/skribbl/LobbyActivity.kt
+++ app/src/main/java/com/example/skribbl/LobbyActivity.kt
@@ -1,8 +1,13 @@
 package com.example.skribbl
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toDrawable
 import kotlinx.android.synthetic.main.lobby_activity.*
 import java.net.Inet4Address
 import java.net.NetworkInterface
@@ -11,6 +16,7 @@ import java.net.Socket
 import java.util.*
 import kotlin.concurrent.thread
 
+
 class LobbyActivity : AppCompatActivity() {
     private var ss = ServerSocket(9999)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,18 +24,37 @@ class LobbyActivity : AppCompatActivity() {
         setContentView(R.layout.lobby_activity)
         ipText.text = "IP address to share : " + getIpv4HostAddress()
         Log.d("toto", "ip : " + getIpv4HostAddress())
+        var avatarImg = findViewById<ImageView>(R.id.player2)
+        var res: ByteArray
+        var oui: Boolean = true
+        var bmp: Bitmap
         thread {
             var client: Socket = ss.accept()
             val test = if (client == null) "fiou" else "tor"
-            var reader = Scanner(client.getInputStream())
+
+            var reader = client.getInputStream()
             var writer = client.getOutputStream()
             Log.d("toto", test)
-            var res = reader.nextLine()
+            res = reader.readBytes()
             writer.write("hi from SERVER".toByteArray())
+            bmp = BitmapFactory.decodeByteArray(res, 0, res.size)
             Log.d("toto", "sent hi")
             Log.d("toto", "Client connected")
             Log.d("toto", "res: $res")
+            avatarImg.setImageBitmap(bmp)
+        }.join()
+
+        launchButton.setOnClickListener {
+            /*
+                envoyer l'instruction pour changer d'activity
+             */
+            val intent = Intent (this, GameActivity::class.java)
+            startActivity(intent)
         }
+
+
+
+
     }
 
     fun getIpv4HostAddress(): String {
@@ -41,3 +66,6 @@ class LobbyActivity : AppCompatActivity() {
         return ""
     }
 }
+
+
+
--- app/src/main/java/com/example/skribbl/MenuActivity.kt
+++ app/src/main/java/com/example/skribbl/MenuActivity.kt
@@ -1,14 +1,19 @@
 package com.example.skribbl
 
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.graphics.drawable.toBitmap
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_menu.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.ByteArrayOutputStream
 import java.net.Socket
 import java.util.*
 
@@ -18,8 +23,17 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-
+        val avatarImg = findViewById<ImageView>(R.id.avatarImage)
+        val bmp: Bitmap = avatarImg.drawable.toBitmap()
+        val bos: ByteArrayOutputStream = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.PNG, 0, bos)
+        val arr: ByteArray = bos.toByteArray()
+        //val ii = avatarImg.drawable.toString()
+        //Log.d("toto", "$ii")
+        val avatarImgName = avatarImg.tag.toString()
         joinLobby.setOnClickListener {
+
+            //val oui = avatarImg.getTag().toString()
             val address = ipJoin.text.toString()
             val port = 9999
             /*
@@ -35,8 +49,9 @@ class MenuActivity : AppCompatActivity() {
             }
             */
             active = true
+
             CoroutineScope(Dispatchers.IO).launch {
-                client(address, port)
+                client(address, port, arr)
             }
         }
 
@@ -46,12 +61,12 @@ class MenuActivity : AppCompatActivity() {
         }
     }
 
-    private suspend fun client(address: String, port: Int) {
+    private suspend fun client(address: String, port: Int, messge: ByteArray) {
         val connection = Socket(address, port)
         val writer = connection.getOutputStream()
         val reader = Scanner(connection.getInputStream())
         val msg = "hello"
-        writer.write(msg.toByteArray())
+        writer.write(messge)
         //active = false
         /*
         while (active) {
